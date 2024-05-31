d32fc530477a836ff678928ee5f62d01d9e92e46
yw5123 <onestar5123@naver.com>
2024-05-31 11:44:46 +0900

    데이터생성기(user: 이름 리스트 추가, 생일 datetime 사용)

--- 3.PYTHON/10.project/data_generator/generators/birthdate_generator.py
+++ 3.PYTHON/10.project/data_generator/generators/birthdate_generator.py
@@ -1,9 +1,12 @@
 import random
+from datetime import datetime
 
 class BirthdateGenerator:
     def generate_birthdate(self):
+        now = datetime.now()
         year = random.randint(1960,2010)
         month = random.randint(1,12)
+
         if month == 2:
             day = random.randint(1,28)
         elif month == 4 or month == 6 or month == 9 or month == 11:
@@ -11,6 +14,6 @@ class BirthdateGenerator:
         else:
             day = random.randint(1,31)
         
-        age = 2024 - year
+        age = now.year - year
 
         return age, f"{year}-{month:02d}-{day:02d}"
