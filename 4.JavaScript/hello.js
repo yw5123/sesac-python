31504a818408bf0fff3a10c61d9636e241b58572
yw5123 <onestar5123@naver.com>
2024-06-03 16:40:42 +0900

    0603 수업 (JavaScript)

---
+++ 4.JavaScript/hello.js
@@ -0,0 +1,16 @@
+function sayHello() {
+    console.log('Hello');
+}
+function sayPrint() {
+    print();
+}
+
+console.log('콘솔로그') // 콘솔창에 로그를 출력한다
+document.write('Hello, World'); // 문서에 글자를 쓴다
+document.write('<P>');
+document.write('헬로우 어게인');
+document.write('<H2>헤딩 추가하기</H2>')
+
+for (var i = 1; i <= 5; i++) {
+    document.write("<h3>숫자: "+ i + "</h3>");
+}
